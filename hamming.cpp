4fac426d3aa19841d7ea1aec573e81f5a6688edd
PastMoments <3858420+PastMoments@users.noreply.github.com>
2020-10-21 01:44:05 -0400

    calculate and consider less values

--- hamming.cpp
+++ hamming.cpp
@@ -1,6 +1,6 @@
 // Create a sorted list of Hamming numbers, i.e. numbers of the form 2^i * 3^j * 5^k,  (i,j,k >= 0)
 // Implemented using a Generator Class
-
+// Note: Requires C++17 
 #include <iostream>
 #include <set>
 #include <vector>
@@ -12,21 +12,33 @@ class Hamming {
     typedef unsigned long NumberType;
     typedef std::set<NumberType> NumberSetType;
     static NumberSetType hamming_nums;
+    constexpr static NumberType primes[3] = {2,3,5};
+    constexpr static size_t num_primes = sizeof(primes)/sizeof(NumberType);
 
     NumberSetType::size_type index = 0;
 public:
     Hamming() {}
     NumberType operator()() {
-        // create new numbers and merge
-        if(hamming_nums.size() <= index){
-            NumberSetType new_set;
-            std::transform(hamming_nums.upper_bound(*hamming_nums.rbegin()/2), hamming_nums.end(), std::inserter(new_set, new_set.end()), [](NumberType i) { return (ULONG_MAX/2 > i ?  2 * i : 1); });
-            std::transform(hamming_nums.upper_bound(*hamming_nums.rbegin()/3), hamming_nums.end(), std::inserter(new_set, new_set.end()), [](NumberType i) { return (ULONG_MAX/3 > i ?  3 * i : 1); });
-            std::transform(hamming_nums.upper_bound(*hamming_nums.rbegin()/5), hamming_nums.end(), std::inserter(new_set, new_set.end()), [](NumberType i) { return (ULONG_MAX/5 > i ?  5 * i : 1); });
-            NumberType new_val = *new_set.upper_bound(*hamming_nums.rbegin());
+        while(hamming_nums.size() <= index){ // create new hamming numbers until we have enough
+            NumberType vals[num_primes] = {0};
+            for(size_t i = 0; i < num_primes; ++i) {
+                NumberType p = primes[i];
+                NumberType n = *hamming_nums.upper_bound(*hamming_nums.rbegin()/p);
+                if(ULONG_MAX/p > n) { // overflow checking
+                    vals[i] = p * n;
+                }
+                // 0 will mark overflowed values
+            }
+
+            // custom comparison function, like operator< except 0 is considered infinitely large
+            NumberType new_val = *std::min_element(vals, vals+num_primes, [](NumberType i, NumberType j) { return (i == 0 ? false : (j == 0 ? true : i < j)); });
+            if(new_val == 0) {
+                return 0; // we've reached the largest value achievable with this int type
+            }
             hamming_nums.insert(new_val);
         }
-        NumberType val = *std::next(hamming_nums.begin(), index);        
+
+        NumberType val = *std::next(hamming_nums.begin(), index);
         ++index;
         return val;
     }
@@ -34,11 +46,11 @@ public:
 Hamming::NumberSetType Hamming::hamming_nums = {1};
 
 int main() {
-  std::vector<unsigned long> hammingVec(512);
-  std::generate(hammingVec.begin(), hammingVec.end(), Hamming());
-
-  for(const auto& i : hammingVec) {
-      std::cout << i << " ";
-  }
-  std::cout << std::endl;
+    std::vector<unsigned long> hammingVec(512);
+    std::generate(hammingVec.begin(), hammingVec.end(), Hamming());
+  
+    for(const auto& i : hammingVec) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
 }
