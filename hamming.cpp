ff83fec6d8dd3207b319fe3e0a6603d3b2c4227f
PastMoments <3858420+PastMoments@users.noreply.github.com>
2020-10-20 21:46:46 -0400

    use typedefs

--- hamming.cpp
+++ hamming.cpp
@@ -8,32 +8,34 @@
 #include <algorithm>
 #include <climits>
 
-class Hamming {        
-    static std::set<unsigned long> hamming_nums;
-    static std::set<unsigned long>::size_type largest_index; // largest confirmed pre-calculated index
+class Hamming {
+    typedef unsigned long NumberType;
+    typedef std::set<NumberType> NumberSetType;
+    static NumberSetType hamming_nums;
+    static NumberSetType::size_type largest_index; // largest confirmed pre-calculated index
 
-    std::set<unsigned long>::size_type index = 0;
+    NumberSetType::size_type index = 0;
 public:
     Hamming() {}
-    unsigned long operator()() {
+    NumberType operator()() {
         // create new numbers and merge
         if(largest_index <= index){
-            std::set<unsigned long> new_set;
-            std::transform(hamming_nums.begin(), hamming_nums.end(), std::inserter(new_set, new_set.end()), [](unsigned long i) { return (ULONG_MAX/2 > i ?  2 * i : 1); });
-            std::transform(hamming_nums.begin(), hamming_nums.end(), std::inserter(new_set, new_set.end()), [](unsigned long i) { return (ULONG_MAX/3 > i ?  3 * i : 1); });
-            std::transform(hamming_nums.begin(), hamming_nums.end(), std::inserter(new_set, new_set.end()), [](unsigned long i) { return (ULONG_MAX/5 > i ?  5 * i : 1); });
-            unsigned long new_val = *new_set.upper_bound(*hamming_nums.rbegin());
+            NumberSetType new_set;
+            std::transform(hamming_nums.begin(), hamming_nums.end(), std::inserter(new_set, new_set.end()), [](NumberType i) { return (ULONG_MAX/2 > i ?  2 * i : 1); });
+            std::transform(hamming_nums.begin(), hamming_nums.end(), std::inserter(new_set, new_set.end()), [](NumberType i) { return (ULONG_MAX/3 > i ?  3 * i : 1); });
+            std::transform(hamming_nums.begin(), hamming_nums.end(), std::inserter(new_set, new_set.end()), [](NumberType i) { return (ULONG_MAX/5 > i ?  5 * i : 1); });
+            NumberType new_val = *new_set.upper_bound(*hamming_nums.rbegin());
             hamming_nums.insert(new_val);
 
             ++largest_index;
         }
-        std::set<unsigned long>::const_iterator current = std::next(hamming_nums.begin(), index);        
+        NumberSetType::const_iterator current = std::next(hamming_nums.begin(), index);        
         ++index;
         return *current;
     }
 };
-std::set<unsigned long> Hamming::hamming_nums = {1};
-std::set<unsigned long>::size_type Hamming::largest_index = 0;
+Hamming::NumberSetType Hamming::hamming_nums = {1};
+Hamming::NumberSetType::size_type Hamming::largest_index = 0;
 
 int main() {
   std::vector<unsigned long> hammingVec(128);
